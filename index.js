70969d4cc8fd23e1622384f440fae5edaf550f57
Martin Heidegger <martin.heidegger@gmail.com>
2015-11-23 02:10:11 +0900

    Standard formatting.

--- index.js
+++ index.js
@@ -1,185 +1,173 @@
-#! /usr/bin/env node
+#!/usr/bin/env node
 
-var fs       = require('fs'),
-    childProcess = require('child_process');
-    minimist = require("minimist"),    
-    workshops = require('./workshops.js'),
-    checkNpm = require('./check-npm.js');
+var fs = require('fs'),
+  childProcess = require('child_process'),
+  minimist = require('minimist'),
+  workshops = require('./workshops.js'),
+  checkNpm = require('./check-npm.js')
 
 var allPackages = workshops.getAll(),
-    args = minimist(process.argv);
+  args = minimist(process.argv)
 
-allInstallCommands = workshops.getInstallCommands();
-var installedList = checkNpm.installedList(allInstallCommands);
+allInstallCommands = workshops.getInstallCommands()
+var installedList = checkNpm.installedList(allInstallCommands)
 
-
-var indexes  = [],i=0;
-for(attr in args){
-    indexes[i] = attr;
-    i++;
+var indexes = [], i = 0
+for (attr in args) {
+  indexes[i] = attr
+  i++
 }
 
+function install (moduleName) {
+  // check if it is a module or a list of modules 
+  var command = ''
+  console.log('type is : ' + typeof (moduleName))
+  if (typeof (moduleName) === 'string') {
+    command = 'sudo npm install -g ' + moduleName
+  } else if ((typeof (moduleName) !== 'boolean') && (moduleName.length !== 0)) {
+    command = 'sudo npm install -g ' + moduleName.join(' ')
+  } else {
+    console.log('No workshop to be installed')
+  }
 
-function install(moduleName){    
+  // console.log(command)
 
-    // check if it is a module or a list of modules 
-    var command = '';
-    console.log('type is : '+typeof(moduleName));
-    if(typeof(moduleName) === 'string'){
-        command = 'sudo npm install -g '+ moduleName;
-    }else if((typeof(moduleName) !== 'boolean') && (moduleName.length !== 0)){        
-        command = 'sudo npm install -g '+ moduleName.join(' ');
-    }else{
-        console.log('No workshop to be installed');
+  childProcess.exec(command, function (err, stdout, stderr) {
+    if (err) {
+      throw err
     }
-
-    // console.log(command);
-
-    childProcess.exec(command,function(err,stdout,stderr){
-        if(err){
-            throw err;            
-        }
-        console.log(stdout);
-    }); 
+    console.log(stdout)
+  })
 }
 
-function remove(moduleName){    
+function remove (moduleName) {
+  // check if it is a module or a list of modules 
+  var command = ''
+  if (typeof (moduleName) === 'string') {
+    command = 'sudo npm remove -g ' + moduleName
+  } else if ((typeof (moduleName) !== 'boolean') && (moduleName.length !== 0)) {
+    command = 'sudo npm remove -g ' + moduleName.join(' ')
+  }
 
-    // check if it is a module or a list of modules 
-    var command = '';
-    if(typeof(moduleName)  === 'string'){
-        command = 'sudo npm remove -g '+ moduleName;
-    }else if((typeof(moduleName) !=='boolean' ) && (moduleName.length !== 0)){        
-        command = 'sudo npm remove -g '+ moduleName.join(' ');
-    }    
-
-    childProcess.exec(command,function(err,stdout,stderr){
-        if(err){
-            throw err;            
-        }
-        console.log(stdout);
-    }); 
+  childProcess.exec(command, function (err, stdout, stderr) {
+    if (err) {
+      throw err
+    }
+    console.log(stdout)
+  })
 }
 
+console.log(indexes[1])
 
-switch(indexes[1]){
-    case 'list':
-        if(args.installed){     
-            // console.log('display installed list ');   
-            displayList(allInstallCommands,'install',["You have","nodeschool workshops installed"]);
-        }else if(args.notinstalled){
-            // console.log('display not installed list ');   
-            displayList(allInstallCommands,'notinstall',["You have","nodeschool workshops  not installed :"]);        
-        }else{
-            displayList(allInstallCommands,'',["There is yes yes","nodeschool workshops  in total :"]);        
+switch (indexes[1]) {
+  case 'list':
+    if (args.installed) {
+      // console.log('display installed list ');   
+      displayList(allInstallCommands, 'install', ['You have', 'nodeschool workshops installed'])
+    } else if (args.notinstalled) {
+      // console.log('display not installed list ');   
+      displayList(allInstallCommands, 'notinstall', ['You have', 'nodeschool workshops  not installed :'])
+    } else {
+      displayList(allInstallCommands, '', ['There is yes yes', 'nodeschool workshops  in total :'])
+    }
+    break
+  case 'search':
+    var searchPattern = args.search,
+      retrievedElements = checkNpm.search(searchPattern, workshops.getInstallCommands())
+
+    if (args.installed) {
+      displayList(retrievedElements, 'install', ['You have', 'nodeschool workshops installed'])
+    } else if (args.notinstalled) {
+      displayList(retrievedElements, 'notinstall', ['You have', 'nodeschool workshops not installed :'])
+    } else {
+      displayList(retrievedElements, '', ['There is', 'nodeschool workshops  in total :'])
+    }
+    break
+  case 'install':
+
+    var installPattern = args.install
+    if (typeof (installPattern) === 'string') {
+      // search for the workshop in our list             
+      var moduleExists = checkNpm.isModule(allInstallCommands, installPattern)
+
+      // if it exists 
+      if (moduleExists) {
+        if (checkNpm.isModule(installedList, installPattern)) {
+          // module exists and already installed, display a message that says it is already installed  
+          console.log('that module (' + installPattern + ') is already installed on your  machine :)')
+        } else {
+          // if it does exist   and not installed, then install 
+          // here we get the the install phase
+          console.log('the module exists and is ready to be installed')
+          console.log('-------------------------------------------------')
+          // install the module asynchronously
+          install(installPattern)
         }
-        break;
-    case 'search':
-        var searchPattern = args.search,
-        retrievedElements = checkNpm.search(searchPattern,workshops.getInstallCommands());
+      } else {
+        // module doesn't exist in our db 
+        console.log('Sorry, this workshop(' + installPattern + ') doesnt exist')
+      }
 
-        if(args.installed){        
-            displayList(retrievedElements,'install',["You have","nodeschool workshops installed"]);
-        }else if(args.notinstalled){
-            displayList(retrievedElements,'notinstall',["You have","nodeschool workshops  not installed :"]);        
-        }else{
-            displayList(retrievedElements,'',["There is","nodeschool workshops  in total :"]);        
-        } 
-        break;  
-    case 'install':
+    } else if (installPattern === true) {
+      if (typeof (args.search) === 'boolean') {
+        console.log('Please, provide the name of the workshop after --search ')
+      } else {
+        // you want to install a group of workshops that match a regex 
+        var searchPattern = args.search
+        // retrieved list contains  both installed and not installed workshops
+        retrievedElements = checkNpm.search(searchPattern, workshops.getInstallCommands())
+        retrievedElements = checkNpm.notInstalledList(retrievedElements)
 
-        var installPattern = args.install;      
-        if(typeof(installPattern) === 'string'){
+        console.log('you are about to install the following ' + retrievedElements.length + ' workshops:')
+        console.log('---------------------------------------------------------------')
+        install(retrievedElements)
+      }
 
-            // search for the workshop in our list             
-            var moduleExists = checkNpm.isModule(allInstallCommands,installPattern);
+    // console.log('Please, provide the name of the workshop after --install ')
+    }
+    break
+  case 'remove':
 
-           
-            // if it exists 
-            if(moduleExists){
-                if(checkNpm.isModule(installedList,installPattern)){
-                    // module exists and already installed, display a message that says it is already installed  
-                    console.log('that module (' + installPattern + ') is already installed on your  machine :)');                     
-                }else{
+    var removePattern = args.remove
+    if (typeof (removePattern) === 'string') {
+      // search for the workshop in our  instaled list             
+      var moduleExists = checkNpm.isModule(installed, installPattern)
 
-                    // if it does exist   and not installed, then install 
-                    // here we get the the install phase
-                    console.log('the module exists and is ready to be installed');                    
-                    console.log('-------------------------------------------------');
-                    // install the module asynchronously
-                    install(installPattern)                                        
-                }
-            }else{
-                // module doesn't exist in our db 
-                console.log('Sorry, this workshop(' + installPattern + ') doesnt exist'); 
-            }            
+      // if it exists 
+      if (moduleExists) {
+        // module exists and can be removed,
+        remove(removePattern)
+      } else {
+        // module doesn't exist in our db 
+        console.log('Sorry, this workshop( ' + installPattern + ' ) doesnt exist')
+      }
+    } else if (removePattern === true) {
+      if (typeof (args.search) === 'boolean') {
+        console.log('Please, provide the name of the workshop after --search ')
+      } else {
+        // you want to remove a group of workshops that match a regex 
+        removePattern = args.search
+        // retrieved list contains  just installed workshops
+        retrievedElements = checkNpm.search(removePattern, installedList)
 
-        }else if(installPattern === true){
-
-            if(typeof(args.search) === 'boolean'){
-                console.log('Please, provide the name of the workshop after --search ');
-            }else{
-                // you want to install a group of workshops that match a regex 
-                var searchPattern = args.search;
-                // retrieved list contains  both installed and not installed workshops
-                retrievedElements = checkNpm.search(searchPattern,workshops.getInstallCommands());
-                retrievedElements = checkNpm.notInstalledList(retrievedElements);
-
-                console.log('you are about to install the following ' + retrievedElements.length + ' workshops:');
-                console.log('---------------------------------------------------------------');                
-                install(retrievedElements);
-               
-            }
-
-            // console.log('Please, provide the name of the workshop after --install ');
-        }
-    break;
-    case 'remove':
-
-         var removePattern = args.remove;      
-        if(typeof(removePattern) === 'string'){
-           // search for the workshop in our  instaled list             
-            var moduleExists = checkNpm.isModule(installed,installPattern);
-
-           
-            // if it exists 
-            if(moduleExists){                
-                // module exists and can be removed,
-                remove(removePattern);
-            }else{
-                // module doesn't exist in our db 
-                console.log('Sorry, this workshop( ' + installPattern + ' ) doesnt exist'); 
-            }            
-        }else if(removePattern === true){
-
-            if(typeof(args.search) === 'boolean'){
-                console.log('Please, provide the name of the workshop after --search ');
-            }else{
-                // you want to remove a group of workshops that match a regex 
-                removePattern = args.search;
-                // retrieved list contains  just installed workshops
-                retrievedElements = checkNpm.search(removePattern,installedList);
-                
-                console.log('you are about to remove the following ' + retrievedElements.length + ' workshops:');
-                console.log('---------------------------------------------------------------');
-                console.log(display(retrievedElements));                
-                remove(retrievedElements);               
-            }            
-        }
-    break;
+        console.log('you are about to remove the following ' + retrievedElements.length + ' workshops:')
+        console.log('---------------------------------------------------------------')
+        console.log(display(retrievedElements))
+        remove(retrievedElements)
+      }
+    }
+    break
 }
 
-function display(list){
-    return list.map(function(item){
-                            return "├──" + item;
-                })
-                .join('\n');
+function display (list) {
+  return list.map(function (item) {
+    return '├──' + item
+  }).join('\n')
 }
 
-function displayList(list,type,message){
-    var retrievedList = (type == '')?list:checkNpm[(type == 'install')?'installedList':'notInstalledList'](list);
-        console.log(message[0] + ' ' + retrievedList.length + ' '+message[1]);
-        console.log('-------------------------------------------------')
-        console.log(display(retrievedList)+ '\n');
-
+function displayList (list, type, message) {
+  var retrievedList = (type == '') ? list : checkNpm[(type == 'install') ? 'installedList' : 'notInstalledList'](list)
+  console.log(message[0] + ' ' + retrievedList.length + ' ' + message[1])
+  console.log('-------------------------------------------------')
+  console.log(display(retrievedList) + '\n')
 }
--- workshops.js
+++ workshops.js
@@ -1,69 +1,67 @@
 var data = {
-        core:[
-            {name :"javascripting", value:"javascripting"},
-            {name :"learnyounode", value:"learnyounode"},
-            {name :"git-it", value:"git-it"},
-            {name :"how to npm", value:"how-to-npm"},
-            {name :"Scope Chains & Closures ", value:"scope-chains-closures"},
-            {name :"stream-adventure", value:"stream-adventure"}
-        ],
-        electives:[
-            {name :"functional javascript", value:"functional-javascript-workshop"},
-            {name :"shader school", value:"shader-school"},
-            {name :"level me up scotty!", value:"levelmeup"},
-            {name :"bytewiser", value:"bytewiser"},
-            {name :"expressworks", value:"expressworks"},
-            {name :"bug clinic", value:"bug-clinic"},
-            {name :"make me hapi", value:"makemehapi"},
-            {name :"browserify adventure", value:"browserify-adventure"},
-            {name :"primise it won't hurt", value:"promise-it-wont-hurt"},
-            {name :"intro to webGL", value:"introtowebgl"},
-            {name :"async you", value:"async-you"},
-            {name :"count to 6", value:"count-to-6"},
-            {name :"nodebot workshop", value:"nodeboot-workshop"},
-            {name :"kick off koa", value:"kick-off-koa"},
-            {name :"going native", value:"goingnative"},
-            {name :"lololodash", value:"lololodash"},
-            {name :"planet proto", value:"planetproto"},
-            {name :"learn you couch db", value:"learnyoucouchdb"},
-            {name :"web GL workshop", value:"webgl-workshop"},
-            {name :"es next generation", value:"esnext-generation"},
-            {name :"learn generators", value:"learn-generators"},
-            {name :"test anything", value:"test-anything"},
-            {name :"learn you react", value:"learnyoureact"},
-            {name :"tower of babel", value:"tower-of-babel"},
-            {name :"perfschool", value:"perfschool"},
-            {name :"learnyoumongo", value:"learnyoumongo"},
-            {name :"web audio school", value:"web-audio-school"},
-            {name :"regex adventure", value:"regex-adventure"},
-            {name :"torrential", value:"torrential"},
-            {name :"learn sass", value:"learn-sass"},
-            {name :"thinking in react", value:"thinking-in-react"},
-            {name :"pattern lab workshop", value:"pattern-lab-workshop"},
-            {name :"post mortem debugging", value:"node-debug-school"}
-        ]
-} ;
-
-
-function getAll(){
-   return  data.core.concat(data.electives);
+  core: [
+    {name: 'javascripting', value: 'javascripting'},
+    {name: 'learnyounode', value: 'learnyounode'},
+    {name: 'git-it', value: 'git-it'},
+    {name: 'how to npm', value: 'how-to-npm'},
+    {name: 'Scope Chains & Closures ', value: 'scope-chains-closures'},
+    {name: 'stream-adventure', value: 'stream-adventure'}
+  ],
+  electives: [
+    {name: 'functional javascript', value: 'functional-javascript-workshop'},
+    {name: 'shader school', value: 'shader-school'},
+    {name: 'level me up scotty!', value: 'levelmeup'},
+    {name: 'bytewiser', value: 'bytewiser'},
+    {name: 'expressworks', value: 'expressworks'},
+    {name: 'bug clinic', value: 'bug-clinic'},
+    {name: 'make me hapi', value: 'makemehapi'},
+    {name: 'browserify adventure', value: 'browserify-adventure'},
+    {name: "primise it won't hurt", value: 'promise-it-wont-hurt'},
+    {name: 'intro to webGL', value: 'introtowebgl'},
+    {name: 'async you', value: 'async-you'},
+    {name: 'count to 6', value: 'count-to-6'},
+    {name: 'nodebot workshop', value: 'nodeboot-workshop'},
+    {name: 'kick off koa', value: 'kick-off-koa'},
+    {name: 'going native', value: 'goingnative'},
+    {name: 'lololodash', value: 'lololodash'},
+    {name: 'planet proto', value: 'planetproto'},
+    {name: 'learn you couch db', value: 'learnyoucouchdb'},
+    {name: 'web GL workshop', value: 'webgl-workshop'},
+    {name: 'es next generation', value: 'esnext-generation'},
+    {name: 'learn generators', value: 'learn-generators'},
+    {name: 'test anything', value: 'test-anything'},
+    {name: 'learn you react', value: 'learnyoureact'},
+    {name: 'tower of babel', value: 'tower-of-babel'},
+    {name: 'perfschool', value: 'perfschool'},
+    {name: 'learnyoumongo', value: 'learnyoumongo'},
+    {name: 'web audio school', value: 'web-audio-school'},
+    {name: 'regex adventure', value: 'regex-adventure'},
+    {name: 'torrential', value: 'torrential'},
+    {name: 'learn sass', value: 'learn-sass'},
+    {name: 'thinking in react', value: 'thinking-in-react'},
+    {name: 'pattern lab workshop', value: 'pattern-lab-workshop'},
+    {name: 'post mortem debugging', value: 'node-debug-school'}
+  ]
 }
 
-function getNames(){
-    return getAll().map(function(item){
-        return item.name;
-    }); 
+function getAll () {
+  return data.core.concat(data.electives)
 }
 
-function getInstallCommands(){
-    return getAll().map(function(item){
-        return item.value;
-    });
+function getNames () {
+  return getAll().map(function (item) {
+    return item.name
+  })
 }
 
-module.exports =  {
-    getAll:getAll,
-    getNames:getNames,
-    getInstallCommands:getInstallCommands
-};
+function getInstallCommands () {
+  return getAll().map(function (item) {
+    return item.value
+  })
+}
 
+module.exports = {
+  getAll: getAll,
+  getNames: getNames,
+  getInstallCommands: getInstallCommands
+}
